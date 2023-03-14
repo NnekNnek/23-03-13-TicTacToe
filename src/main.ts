25bcbbc39c2fb46b96657619210cec59b284003e
Nnek Nnek <hillmar@posteo.de>
2023-03-14 12:09:02 +0100

    finished main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,8 @@ type Player = {
     symbol: "x" |"o";
     score: number
 }
+// pics instead of s/o? make a type called Symbol, x:"./" and o:"./".
+// then add the type Symbol to the type Player (symbol: Symbol)
 
 // 7. TYPE FOR CELL: marked as string or null
 type CellState = {
@@ -42,7 +44,6 @@ function makeGrid(){
             const id = coordtoId([row,col]);
             // coordToId is a function, so 'const id' is bind to the function with turn numbers into a string
             cell.id = id;
-            cell.textContent = id;
 
             // initialising the cell state so it can be tracked
             // they can also be tracked after eventlisteners have been listet for x and o
@@ -56,12 +57,14 @@ function makeGrid(){
 
             cell.addEventListener("click", (event) => {
                 if (!gameEndState){
-                    // whose turn
+                    // whose turn if game is not completed
                     const currentPlayer = players[turn];
+                    // give me player of index[turn] --> initial value of turn = 0
 
                     // player (who's turn it is) adds symbol to the markdBy key for each specific cell
                     const cellState = gameState[id];
                     const isMarked = Boolean(cellState.markedBy);
+                    // converting it into a Booloean to check whether it's marked or not
 
                     if (!isMarked){
                         cellState.markedBy = currentPlayer.name;
@@ -69,11 +72,22 @@ function makeGrid(){
                         cell.innerHTML = `<div class="flex justify-center items-center h-full"><p class="text-xl">${currentPlayer.symbol}</p></div>`;
                        
                         // put winning conditions 
+                        const gameWinner = didIWin();
+                        
+                        if (gameWinner){
+                            // if it equals to true, then...
+                            displayWinner();
+                            gameEndState = true;
+                            return
+                        }
 
                         // next turn
                         turn = (turn + 1) % players.length;
+                        // modulus always give a WHOLE number
+                        // first time: 0+1 % 2 = 1
+                        // outcome is either 0 or 1
                         const nextPlayer = players[turn];
-                        currentPlayerElement.textContent = `The current player is ${nextPlayer}`
+                        currentPlayerElement.textContent = `The current player is ${nextPlayer.name}`
                     }
                 }
             })
@@ -96,6 +110,7 @@ const gridCellStyling = ["h-[200px]", "w-[200px]", "border", "border-black"];
 // 8A. UTILITY FUNCTION TO KEEP TRACK
 function coordtoId(coord: Coordinates): string {
     // parameter (coord) as type (Coordinates) has to be the outcome string as "number-number"
+    // IDs can NOT be numbers! So we have to transform them
     // *string* could also just be `${number}-&{number}`
     const [row, col] = coord;
     // because type Coordinates is [number, number]
@@ -104,11 +119,12 @@ function coordtoId(coord: Coordinates): string {
     return `${row}-${col}`; 
 }
 
-// 8B. do reversed 8 (no clue why)
-function IdToCoordinates(id: `${number}-&{number}`): Coordinates{
+
+// 8B. TAKES IDs (string) AND MAKES THEM INTO COORDINATES (numbers) AGAIN
+function IdToCoordinates(id: `${number}-${number}`): Coordinates{
     const [row, col] = id.split("-")
     return [parseInt(row), parseInt(col)]
-    // parseInt: passes a string into an interger
+    // parseInt(constructor): passes a string into an interger
 }
 
 // 9. GRAB #current-player (paragraph) FROM DOM
@@ -141,6 +157,7 @@ currentPlayerElement.textContent = `The current player is ${players[0].name}`;
 let gameState: Record<string, CellState> = {};
 // tracking the game from the initial state to the end state
 // CellState refers to the type
+// ?
 
 
 // 13. WINNING CONDITIONS
@@ -157,22 +174,53 @@ const winConditions = [
 
 
 // 14. FUNCTION FOR WINNING CONDITION
+function didIWin(){
+    for (const winCondition of winConditions){
+        const [cell1, cell2, cell3] = winCondition.map((id)=> gameState[id])
+        // map is like a for-each loop, goes through each item
+        // 'gameState[id]' access the game state, to see wheter those cells have been markedBy
 
-// function winning() = {
-//     for (let i = 0; i < winConditions.length; i++){
-//         console.log()
-//         if (currentGameState === (...winConditions)){
+        const winner = 
+        cell1.markedBy !=null && 
+        cell2.markedBy !=null && 
+        cell3.markedBy !=null && 
+        // are all 3 cells filled with a symbol
+        cell1.markedBy == cell2.markedBy &&
+        cell1.markedBy == cell3.markedBy;
+        // if all 3 cells have the same symbol
 
-// const = document.getElementById
-// '${players:name}, you've won the game!'
-//         }
-//     }
-// }
-
-// console.log(winConditions[0])
-// const currentGameState = [`${number}-&{number}`, `${number}-&{number}`, `${number}-&{number}]
+        if (winner){
+            // defined in the const above
+            return true
+        }
+    } 
+}
 
 
 
+// 15. DISPLAY A CONGRATS-MESSAGE
+function displayWinner (){
+    currentPlayerElement.textContent = `CONGRACTS ${players[turn].name}, you've won :)`
+}
+
+
+// 16. RESETTING THE GAME TO PLAY AGAIN
+function resetGrid(){
+    while (gameGrid?.lastChild){
+    // if my grid has a child (one of the 9 cells), remove that child (cell)
+    // while-loop
+        gameGrid.removeChild(gameGrid.lastChild)
+    }
+
+    // setting it back to the beginning
+    gameEndState = false;
+    turn = 0;
+    gameState = {}
+    currentPlayerElement.textContent = `The current player is ${players[0].name}`
+    
+    makeGrid();
+}
+
+resetButton?.addEventListener("click", resetGrid);
 
 makeGrid();
